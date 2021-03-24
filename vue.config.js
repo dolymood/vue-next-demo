871ae89b45a1ce5ed65e7db5d503c9a54489c31e
miaodian <miaodian@didiglobal.com>
2021-03-24 17:47:11 +0800

    @vue transpile path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   outputDir: 'docs',
   publicPath: '/vue-next-demo/',
-  transpileDependencies: [/@vue\/.*/, 'vue-router', 'vue']
+  transpileDependencies: [/@vue.*/, 'vue-router', 'vue']
 }
